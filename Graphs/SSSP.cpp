4e7a1f7aeaa9cb0421c59d74202d3cdb787ae68e
AthSat <atharvsathe28704@gmail.com>
2025-07-03 12:13:29 +0530

    Add Single Source Shortest Path (SSSP) algorithm implementation for directed acyclic graphs (DAG)

---
+++ Graphs/SSSP.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Single Source Shortest Path in DAG
+
+int dfs(vector<vector<pair<int, int>>>& adjList, int i, vector<bool>& visited,
+        int loc, vector<int>& order) {
+  visited[i] = true;
+
+  for (auto& nbr : adjList[i]) {
+    if (!visited[nbr.first]) {
+      loc = dfs(adjList, nbr.first, visited, loc, order);
+    }
+  }
+
+  order[loc] = i;
+  return loc - 1;
+}
+
+vector<int> topoSort(vector<vector<pair<int, int>>>& adjList) {
+  int nodeCount = adjList.size();
+
+  vector<int> order(nodeCount, 0);
+  vector<bool> visited(nodeCount, false);
+
+  int loc = nodeCount - 1;
+  for (int i = 0; i < nodeCount; i++) {
+    if (!visited[i]) {
+      loc = dfs(adjList, i, visited, loc, order);
+    }
+  }
+
+  return order;
+}
+
+vector<int> shortestPath(int V, int E, vector<vector<int>>& edges, int start) {
+  // Build Graph
+  vector<vector<pair<int, int>>> adjList(V);
+  for (auto& edge : edges) {
+    adjList[edge[0]].push_back({edge[1], edge[2]});
+  }
+
+  vector<int> order = topoSort(adjList);
+
+  vector<int> distance(V, INT_MAX);
+  distance[start] = 0;
+
+  for (int i = 0; i < V; i++) {
+    int node = order[i];
+    if (distance[node] == INT_MAX) continue;
+
+    for (auto& nbr : adjList[node]) {
+      distance[nbr.first] =
+          min(distance[nbr.first], distance[node] + nbr.second);
+    }
+  }
+
+  for (int& i : distance)
+    if (i == INT_MAX) i = -1;
+  return distance;
+}
+
+int main() { return 0; }
